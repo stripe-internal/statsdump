26674318ceb8081ee79d2fec68f65f94e8a40c24
Ray Bejjani <rb@stripe.com>
2015-10-07 13:53:49 +0200

    Jankily add TCP parse support
    We assume all streams are well formed and accumulate data when we cannot
    parse. This seemed to work!

--- statsdump.py
+++ statsdump.py
@@ -6,17 +6,18 @@ from collections import defaultdict
 from operator import itemgetter
 
 
-def parse_ip4_udp(buf):
+def parse_ip4_packet(buf):
     eth = dpkt.ethernet.Ethernet(buf)
     if eth.type != dpkt.ethernet.ETH_TYPE_IP:
         raise Exception('unhandled type: %s' % eth.type)
     
     ip = eth.data
-    if ip.p != dpkt.ip.IP_PROTO_UDP:
+    if ip.p not in [dpkt.ip.IP_PROTO_UDP, dpkt.ip.IP_PROTO_TCP]:
         raise Exception('unhandled protocol: %s' % ip.p)
+    
 
-    udp = ip.data
-    return socket.inet_ntoa(ip.src), udp.data
+    packet_data = ip.data
+    return socket.inet_ntoa(ip.src), ip.p == dpkt.ip.IP_PROTO_UDP, packet_data.data
 
 
 
@@ -58,23 +59,33 @@ def main():
     keys = defaultdict(int)
     prefixes = defaultdict(int)
 
-    total = 0
+    total, total_udp, total_tcp = 0,0,0
+    error_skipped = 0
+    data = ""
     for buf in pcap_buffers(sys.stdin):
         try:
-            ip, data = parse_ip4_udp(buf)
+            ip, is_udp, this_data = parse_ip4_packet(buf)
+            data += this_data
         except dpkt.dpkt.NeedData:
             continue
-        
-        key, metrictype = parse_statsd(data)
-        hosts[ip] += 1
-        keys[key] += 1
+       
+        try:
+            key, metrictype = parse_statsd(data)
+            hosts[ip] += 1
+            keys[key] += 1
 
-        parts = key.split('.')
-        for prefix in ['.'.join(parts[:n]) for n in range(1, len(parts)+1)]:
-            prefixes[prefix] += 1
+            parts = key.split('.')
+            for prefix in ['.'.join(parts[:n]) for n in range(1, len(parts)+1)]:
+                prefixes[prefix] += 1
 
-        total += 1
+            total += 1
+            if is_udp: total_udp +=1
+            else: total_tcp +=1
+            data = ''
+        except:
+            error_skipped +=1
 
+    print("Skipped {}/{} packets. {} UDP packets, {} TCP packets.".format(error_skipped, total, total_udp, total_tcp))
     print_table('hosts', hosts, 20, total)
     print_table('keys', keys, 20, total)
     print_table('prefixes', prefixes, 20, total, False)
